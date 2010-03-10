a97e53fe88d8dfc713f5a318284cd720233875d8
myturntohide <myturntohide@2e0cd1e4-2c52-11df-b039-17fb639e06ba>
2010-03-10 16:22:52 +0000

    Full upload of initial build including gestures starter interface.

---
+++ WiiMoteControl/trunk/WiiMoteControl/Gesture.vb
@@ -0,0 +1,3 @@
+﻿Public Class Gesture
+
+End Class
---
+++ WiiMoteControl/trunk/WiiMoteControl/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WiiMoteControl")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("InK - Gaming")> 
+<Assembly: AssemblyProduct("WiiMoteControl")> 
+<Assembly: AssemblyCopyright("Tom Piddock")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("11664e9e-c7b2-4bb6-8506-faa86c394e79")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
