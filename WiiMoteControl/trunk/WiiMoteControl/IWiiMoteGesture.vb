46f667b830b75444c7eb552e5f874becdf9aef1d
myturntohide <myturntohide@2e0cd1e4-2c52-11df-b039-17fb639e06ba>
2010-03-10 16:22:52 +0000

    Full upload of initial build including gestures starter interface.

---
+++ WiiMoteControl/trunk/WiiMoteControl/IWiiMoteGesture.vb
@@ -0,0 +1,25 @@
+﻿
+Public Interface IWiiMoteGesture
+    Enum Gesture
+        FlickRight = 0
+        FlickLeft = 1
+        FlickUp = 2
+        FlickDown = 3
+    End Enum
+
+    'USE a queue to read off the gesture values.
+    'Each time a gesture value is received, the value is added to the queue, the queue
+    'is scanned for matching the gesture and returns true if a match is found, else
+    'returns nothing.
+
+    Property InputQueue() As Queue(Of Double)
+
+
+    Event GestureMatchFound(ByVal Gesture As IWiiMoteGesture.Gesture)
+
+    Function ScanForGesture(ByVal InputQueue As Queue) As Boolean
+
+    Sub Update(ByVal NewInputValue As Double)
+
+
+End Interface
